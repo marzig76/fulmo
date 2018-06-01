3aff05fac3f18b0631ecf42b4872d8c1392866b3
Mike Marzigliano <marzig76@gmail.com>
2018-06-01 02:06:01 +0000

    initial push

---
+++ fulmo.py
@@ -0,0 +1,58 @@
+from lightning import LightningRpc
+from flask import Flask, request, render_template
+import random
+import qrcode
+app = Flask(__name__)
+
+# Connect to local LN node
+ln = LightningRpc("/tmp/lightning-rpc")
+
+@app.route("/")
+def fulmo():
+	return render_template('index.html')
+
+@app.route("/getinfo/")
+def getinfo():
+	info = ln.getinfo()
+	return prepare(info)
+
+@app.route("/invoice/")
+def invoice():
+	make_qr = request.args.get("qr")
+	satoshis = request.args.get("amount")
+	description = request.args.get("description")
+	invoice = ln.invoice(satoshis, "lbl{}".format(random.random()), description)
+	bolt11 = str(invoice["bolt11"])
+	if make_qr is not None:
+		return bolt11 + qr("lightning:" + bolt11)
+	else:
+		return bolt11
+
+@app.route("/help/")
+def help():
+        help = ln.help()
+        return prepare(help)
+
+@app.route("/listchannels/")
+def listchannels():
+        channels = ln.listchannels()
+        return prepare(channels)
+
+def qr(data): 
+        print "QR DATA: " + data
+        img = qrcode.make(data)
+        filename = "static/qrcodes/" + data  + ".png"
+        img.save(filename)
+        return str("<br /><img src='/" + filename + "'height='200' width='200'/>")
+        
+
+def prepare(data):
+	data_string = ""
+	for key, value in data.iteritems():
+		if isinstance(value, dict):	
+			data_string = prepare(value)
+		else:
+			data_string = data_string + key + ": " + str(value) + "<br />"
+
+	return data_string
+
---
+++ static/fulmo.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+	console.log('Document Ready');
+
+	refresh();
+
+	// create invoice button event
+	$('#createInvoice').click(function() {
+		createInvoice();
+	});
+
+        // help button event
+        $('#help').click(function() {
+                help();
+        });
+
+	// refresh statuses
+	window.setInterval(refresh, 30000);
+});
+
+function refresh(){
+	console.log("refresh");	
+	getinfo();
+	listchannels();
+}
+
+function getinfo(){
+        $.get( "getinfo/", function( data ) {
+                $('#getinfoText').html(data);
+		console.log( "LN node stats: " + data );
+        });	
+}
+
+function listchannels(){
+        $.get( "listchannels/", function( data ) {
+                $('#channelText').html(data);
+                console.log( "LN list channels: " + data );
+        });
+}
+
+function createInvoice(){
+	var amount = $('#invoiceAmount').val();
+	var description = $('#invoiceDescription').val();
+	var invoiceURL = "invoice/?amount=" + amount + "&description=" + description;
+
+	if ($('#invoiceQR').is(':checked')){
+		invoiceURL = invoiceURL + "&qr";
+	}
+
+	$.get( invoiceURL, function( data ) {
+		$('#invoiceText').html(data);
+		console.log( "Invoice: " + data );
+        });
+}
+
+function help(){
+	$.get( "help/", function( data ) {
+		console.log( "LN Help: " + data );
+	});
+}
